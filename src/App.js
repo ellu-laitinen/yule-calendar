31be78ff32d98b2210986009fd56bcd058540e65
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-09-22 18:49:43 +0300

    add code for basic calendar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { StyledApp } from './AppStyles'
+import { createCalendar } from './helpers'
+import Hatch from './Hatch';
 
 
+const GlobalStyle = createGlobalStyle`
+body {
+  background:  center / cover url('https://source.unsplash.com/7VOyZ0-iO0o')  ;
+  margin: 0;
+}`
 
 function App() {
+  const [hatches, setHatches] = useState([]);
+
+  useEffect(() => {
+    hatches.length && localStorage.setItem('calendar', JSON.stringify(hatches));
+  }, [hatches])
+
+  useEffect(() => {
+    const calendar = localStorage.calendar
+      ? JSON.parse(localStorage.calendar)
+      : createCalendar();
+
+    setHatches(calendar);
+  }, []);
+
+  // store calendar in local storage
+
+  /**/
+
+  const handleFlipHatch = id => {
+    const updatedHatches = hatches.map(hatch =>
+      hatch.id === id ? { ...hatch, open: !hatch.open } : hatch
+    );
+    setHatches(updatedHatches)
+    console.log(id)
+  }
   return (
-    <div className="App">
-      this is going to be a YULE calendar
-    </div>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        {hatches.map(hatch =>
+          <Hatch
+            key={hatch.id}
+            hatchData={hatch}
+            handleClick={handleFlipHatch}
+
+          />)}
+      </StyledApp>
+
+    </>
   );
 }
 

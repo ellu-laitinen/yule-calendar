79ee27da57a54143f1e5f747c536e8912d0dd9e2
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-09-24 14:28:46 +0300

    add overlay

--- src/Hatch.js
+++ src/Hatch.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { StyledHatch } from './HatchStyles'
 
+const overlayOn = () => {
+    document.getElementById("overlay").style.display = "block";
+
+}
+
+const overlayOff = () => {
+    document.getElementById("overlay").style.display = "none";
+}
+
 const Hatch = ({ hatchData: { id, nr, text, img, open }, handleClick }) => (
-    <StyledHatch background={img} onClick={() => handleClick(id)}>
-        <div className={open ? "front open" : "front"}>
-            <p>{nr}</p>
+    <StyledHatch background={img}>
+        <div onClick={() => handleClick(id)} className={open ? "front hatch open" : "front hatch"}>
+            <p >{nr}</p>
         </div>
-        <div className={open ? "back open" : "back"}>
-            <p>{text}</p>
+        <div className={open ? "back hatch open" : "hatch back"}>
+            <p onClick={() => handleClick(id)}>{text}</p>
+            <button onClick={overlayOn}>show overlay</button>
         </div>
+        <div id="overlay" >
+            <div className="hatch-content">
+                <h1 background={img}>hello! {text}</h1>
+                <button className="closebutton" onClick={overlayOff}>close</button>
+            </div>
+
+        </div>
+
     </StyledHatch>
 )
 
